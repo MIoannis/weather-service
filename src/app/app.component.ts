c3f304ba1f32b47ce6a4c5caa72adefe21a13c1b
Ioannis <steam983@gmail.com>
2020-05-10 18:53:13 +0300

    added animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,56 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {SessionService} from './Akita/session.service';
 import {SessionQuery} from './Akita/session.query';
 import {NgForm} from '@angular/forms';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('moveUp', [
+      state('center', style({
+        height: '90%',
+      })),
+      state('up', style({
+        height: '30%',
+      })),
+      transition('center => up', [
+        animate('1s ease-in-out')
+      ]),
+    ]),
+    trigger('changeOpacity', [
+      state('zero', style({
+        opacity: 0
+      })),
+      state('full', style({
+        opacity: 1
+      })),
+      transition('zero => full', [
+        animate('1.2s')
+      ]),
+    ]),
+    trigger('insertTrigger', [
+      transition(':enter', [
+        style({top: '125%'}),
+        animate('1s ease-out', style({ left: '35%', top: '45%' })),
+      ]),
+    ])
+  ]
 })
 export class AppComponent {
+  animVar = true;
+  kek = false;
   weatherdata: any;
 
   constructor(private sessionService: SessionService,
-              public sessionQuery: SessionQuery) { }
+              public sessionQuery: SessionQuery) {
+  }
 
   chooseTown(f: NgForm) {
     this.sessionService.updateData(f.value.town);
+    this.animVar = false;
+    this.kek = true;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
@@ -9,13 +10,16 @@ import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         HttpClientModule,
         AppRoutingModule,
         environment.production ? [] : AkitaNgDevtools,
